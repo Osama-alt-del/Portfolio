bfbc944c3f22defc3e3e5b4b9eeca90c4cca055b
Osama-alt-del <abdalghanyosama@gmail.com>
2025-09-08 22:51:19 +0930

    Fixing the centering problem and title WIP
     - Got rid of align-items and manually positioned the items so that they are always on the top
     - Workin on making the icon and the title smaller using animations when they are open WIP

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 
 function openProjects(projectCard) {
-  console.log(projectCard.offsetHeight);
   if (projectCard.offsetHeight == 165) {
     projectCard.style.animation = "openProjects 1.5s forwards";
   }
   else if (projectCard.offsetHeight == 500) {
-    console.log("running");
     projectCard.style.animation = "closeProjects 1.5s forwards";
+  }
+}
+
+function getSmaller(projectCard){
+  const childImg = projectCard.querySelector('img');
+  const childh1 = projectCard.querySelector('h1');
+  if (childImg.offsetHeight == 100) {
+    childImg.style.animation = "getSmaller 1.5s forwards";
   }
 }
