e4972e8524e6fa0877bbc28d770d24e5fba15788
Osama-alt-del <abdalghanyosama@gmail.com>
2025-09-09 13:15:47 +0930

    Project cards open properly
     - Made it so that the icons and the title get smaller when the project card opens
     - Faced a problem with only the html icon getting smaller on the html, css, and js project card, but that was resolved by checking how many images are inside the project card using querySelectorAll(). In the case that the length is greater than 1, the animation is applied for every images.

--- script.js
+++ script.js
@@ -9,9 +9,32 @@ function openProjects(projectCard) {
 }
 
 function getSmaller(projectCard){
-  const childImg = projectCard.querySelector('img');
+  // I want to check if there is more than 1 image, if there is, then I want to change them all
+  // change to select all images, and then make it so that
+  const childImgs = projectCard.querySelectorAll('img');
   const childh1 = projectCard.querySelector('h1');
+  if (childImgs.length > 1) {
+    childImgs.forEach(img => {
+    if (img.offsetHeight== 100) {
+      img.style.animation = "getSmaller 1.5s forwards";
+      childh1.style.animation = "getSmallerTitle 1.5s forwards";
+    }
+    else if (img.offsetHeight == 50) {
+      img.style.animation = "getLarger 1.5s forwards";
+      childh1.style.animation = "getLargerTitle 1.5s forwards";
+    }
+    });
+  }
+  else {
+  childImg = childImgs[0];
   if (childImg.offsetHeight == 100) {
     childImg.style.animation = "getSmaller 1.5s forwards";
+    childh1.style.animation = "getSmallerTitle 1.5s forwards";
+  }
+  else if (childImg.offsetHeight == 50) {
+    childImg.style.animation = "getLarger 1.5s forwards";
+    childh1.style.animation = "getLargerTitle 1.5s forwards";
+  }
+
   }
 }
